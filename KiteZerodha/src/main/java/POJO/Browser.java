42976d0fa6821dea53bc2e2962cd0faa0f9ecfec
archanawadkar <107843887+archanawadkar@users.noreply.github.com>
2022-06-22 10:05:45 +0530

    Add files via upload

---
+++ KiteZerodha/src/main/java/POJO/Browser.java
@@ -0,0 +1,28 @@
+package POJO;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class Browser {
+
+	public static WebDriver openBrowser () {
+		ChromeOptions options=new ChromeOptions();
+		options.addArguments("--disable-notifications");
+		System.setProperty("webdriver.chrome.driver","C:\\Selenium\\chromedriver_win32 (2)\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://kite.zerodha.com/");
+		driver.manage().window().maximize();
+//		driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);//Implicit wait
+		return driver;
+	}
+	
+	
+	
+	
+	
+	
+	
+}
